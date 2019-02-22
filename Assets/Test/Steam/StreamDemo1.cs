30041f980eeceec5cf210833c903d3196c550bf2
DESKTOP-E3NCM3R\LanShu002 <1156781974@qq.com>
2019-02-22 19:50:54 +0800

    流和Resource笔记

---
+++ Assets/Test/Steam/StreamDemo1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class StreamDemo1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		byte[] buffer = null;
+		string testString = "Stream!Hello world";
+		char[] readCharArray = null;
+		byte[] readBuffer = null;
+		string readString = string.Empty;
+		using (MemoryStream memoryStream=new MemoryStream())
+		{
+			Debug.Log("初始化字符串:"+testString);
+			if (memoryStream.CanWrite)
+			{
+				buffer = Encoding.Default.GetBytes(testString);
+				memoryStream.Write(buffer,0,3);
+				Debug.Log("现在Stream.Postion在第{0}位置"+(memoryStream.Position+1));
+
+				//从刚才结束的位置（当前位置）往后移3位，到第7位
+				long newPositionInStream =memoryStream.CanSeek? memoryStream.Seek(3, SeekOrigin.Current):0;
+
+				Debug.Log("重新定位后Stream.Postion在第{0}位置"+ (newPositionInStream+1));
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
