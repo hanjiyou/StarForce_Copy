727a457e0fe1dcc17a51923e022d6960f83b0142
DESKTOP-E3NCM3R\LanShu002 <1156781974@qq.com>
2019-02-27 10:26:08 +0800

    复习笔记 发射射线的几种方式

---
+++ Assets/Test/TestRay/ExecuteRay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ExecuteRay : MonoBehaviour
+{
+
+	public Camera myCamera;
+	// Use this for initialization
+	void Start()
+	{
+		Ray ray = this.myCamera.ScreenPointToRay(Input.mousePosition);
+		if (EventSystem.current.IsPointerOverGameObject())
+		{
+
+		}
+
+		RaycastHit rcHit;
+		if (Physics.Raycast(ray, out rcHit))
+		{
+
+		}
+
+		PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+		pointerEventData.position = Input.mousePosition;
+		List<RaycastResult> results = new List<RaycastResult>();
+		EventSystem.current.RaycastAll(pointerEventData, results);
+		if (results.Count > 0){
+			
+		}
+
+		Canvas canvas = null;
+		canvas.GetComponent<GraphicRaycaster>().Raycast(pointerEventData, results);
+		//(pointerEventData, results);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
