ddb1c7229f78326e56553b2d8c4cd5a07e8499f5
DESKTOP-PHKB03R\han <1156781974@qq.com>
2019-02-28 22:05:53 +0800

    const

---
+++ Assets/Test/Const/ConstTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ConstTest 
+{
+	private  const int a=2;
+	private   readonly int b=3;
+	 
+	public ConstTest()
+	{
+		b = 2;
+		Debug.Log("b="+b);
+	}
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
